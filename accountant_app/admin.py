1774a30c0dc3b4e35fae92d12937c153afe65107
greenbaskets <gbasketsindia@gmail.com>
2024-07-29 19:30:09 +0530

    29th July

--- accountant_app/admin.py
+++ accountant_app/admin.py
@@ -4,12 +4,16 @@ from import_export.admin import ImportExportModelAdmin
 
 # Register your models here.
 from .models import *
-# Register your models here.
-admin.site.register(AccountTypeGroup)
-admin.site.register(AccountType)
+
+class AccountTypeGroupAdmin(ImportExportModelAdmin,admin.ModelAdmin):
+	...
+admin.site.register(AccountTypeGroup,AccountTypeGroupAdmin)
+class AccountTypeAdmin(ImportExportModelAdmin,admin.ModelAdmin):
+	...
+admin.site.register(AccountType,AccountTypeAdmin)
+admin.site.register(AccountTypeList)
 admin.site.register(Account)
 admin.site.register(ManualJournalVoucher)
 admin.site.register(AccountEntry)
 admin.site.register(AccountTransaction)
-admin.site.register(AccountTypeList)
 
